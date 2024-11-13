a4677e9709585a84825fc7575303ee55c04f8d13
Owen-E22 <owen.eigen@icloud.com>
2024-11-13 16:29:45 -0600

    week 10

--- .github/Program_1.py
+++ .github/Program_1.py
@@ -9,7 +9,6 @@ class triangle:
     def sides_total(self):
         perimeter = self.a + self.b + self.c
         return perimeter
-
 t1 = triangle(3, 4, 5)
 perimeter = t1.sides_total()
 print(f'The perimeter of the triangle is {perimeter}')
